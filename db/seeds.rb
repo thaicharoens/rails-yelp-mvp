d166a4a988080f718cee6150c328e8ad600e2af8
Sunny Thaicharoen <thaicharoens@me.com>
2019-07-23 22:38:18 +0100

    implemented crud features

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurants_list = [{ name: 'Blah', address: '1 Blah Street', category: 'french', phone_number: '111111111'}, { name: 'BlahBlah', address: '2 Blah Street', category: 'french', phone_number: '111111111'}, { name: 'BlahBlahBlah', address: '3 Blah Street', category: 'french', phone_number: '111111111'}, { name: 'BlahBlahBlahBlah', address: '4 Blah Street', category: 'french', phone_number: '111111111'}, { name: 'BlahBlahBlahBlahBlah', address: '5 Blah Street', category: 'french', phone_number: '111111111'}]
+
+restaurants_list.each do |restaurant_hash|
+  Restaurant.create(restaurant_hash)
+end
